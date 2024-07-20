d859702f9bf870cf76b0ec65182775fb17410727
Amey Mohole <112307792+AmeyMohole@users.noreply.github.com>
2024-07-20 23:22:06 +0530

    Add files via upload

---
+++ CONTACT.php
@@ -0,0 +1,211 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Jawahar Education Society's Institute of Technology, Management & Research, Nashik</title>
+  <link rel="icon" href="assets/img/index/fevicon.png" type="image/x-icon">
+  
+  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"  />
+  
+
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">    
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+  <!-- <link rel="stylesheet" href="assets/css/styles.css"> -->
+ 
+  <!-- FEVICON -->
+  <link rel="icon" href="assets/img/index/fevicon.png" type="image/x-icon">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+ 
+  
+  
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> 
+  <!-- Required meta tags -->
+  
+  
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+
+  <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  
+ 
+
+
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<style>
+
+
+
+</style>
+</head>
+	
+	<body>
+	<?php include('header.php') ?>
+	<div class="content container">
+            <div class="page-wrapper">
+              
+                <div class="page-content">
+                    <div class="row">
+                        <article class="contact-form col-lg-8 col-md-7  col-12 page-row">                            
+                            <h4 >Get in touch</h4>
+                         
+                            <form action="submit.php">
+                                <div class="form-group name">
+                                    <label for="name">Name</label>
+                                    <input id="name" type="text" class="form-control rounded-lg" placeholder="Enter your name">
+                                </div>
+                                <div class="form-group email">
+                                    <label for="email">Email<span class="required">*</span></label>
+                                    <input id="email" type="email" class="form-control rounded-lg" placeholder="Enter your email">
+                                </div>
+                                <!-- <div class="form-group phone">
+                                    <label for="phone">Phone</label>
+                                    <input id="phone" type="tel" class="form-control rounded-lg" placeholder="Enter your contact number">
+                                </div> -->
+                                <div class="form-group message">
+                                    <label for="message">Message<span class="required">*</span></label>
+                                    <textarea id="message" class="form-control rounded-lg" rows="6" placeholder="Enter your message here..."></textarea>
+                                </div>
+                                <button type="submit" class="btn btn-danger rounded">Send message</button>
+                            </form>                  
+                        </article>
+                        <aside class="page-sidebar  col-lg-3 offset-lg-1 col-md-4 offset-md-1">
+                            <section class="widget has-divider">
+                                <h4 >Download Prospectus</h4>
+                         
+                                <a class="btn btn-danger rounded" href="#" ><i class="fas fa-download"></i>Download now</a>
+                                <br><br>
+                            </section>  
+                            
+                            <section class="widget has-divider">
+                                <h4 >Postal Address</h4>
+                                <p class="adr">
+                                    <span class="adr-group" style="color:black;">       
+                                        <span class="street-address">  JIT Nashik, Gangapur Road, </span><br>
+                                        <span class="region">Survey No 48, Gowardhan, Gangapur Road,</span><br>
+                                        <span class="postal-code"> Nashik - 422 222.</span><br>
+                                       
+                                    </span>
+                                </p>
+                            </section>     
+                            
+                            <section class="widget">
+                                <h4 >All Enquiries</h4>
+                                <p class="tel" style="color:black;"><i class="fas fa-phone" ></i>Tel: 0253-2970077</p>
+                                <p class="email" style="color:black;"><i class="fas fa-envelope"></i>Email: <a href="#">jitnashik2012@gmail.com</a></p>
+                            </section>   
+                        </aside>
+                    </div>
+					<br><br>	<br><br>	<br><br>	
+					<div class="container">
+    <div class="row">
+	
+        <article class="col-12">
+            <h4 >How to find us:</h4>
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item rounded-lg" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3748.4334471820093!2d73.6871426759095!3d20.03227948137918!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bddedd75b257ee7%3A0xd607636433df8d5f!2sJawahar%20Education%20Society&#39;s%2C%20INSTITUTE%20OF%20TECHNOLOGY%2C%20MANAGEMENT%20AND%20RESEARCH%2C%20NASHIK!5e0!3m2!1sen!2sin!4v1721496823268!5m2!1sen!2sin" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </article>
+    </div>
+</div>
+                </div>
+            </div> 
+        </div>
+    </div>
+	
+	
+<script>
+  AOS.init({
+duration: 2000 // Duration in milliseconds
+});
+</script>
+<footer>
+      <div class="container" data-aos="fade-right">
+          <div class="row">
+              <div class="col-md-4">
+                  <h5>About JIT</h5>
+                  <p>Jawahar Education Society proudly brings top-tier engineering education to Nashik with the establishment of Jawahar Education Society's Institute of Technology, Management & Research, Nashik – widely recognized as JIT Nashik. Here, innovation meets excellence, shaping the leaders of tomorrow.</p>
+              </div>
+              <div class="col-md-4 " >
+                  <div class="row">
+                  <div class="col-sm-6">
+                  <h5>Quick Links</h5>                   
+                  <ul class="list-unstyled">
+                      <li><a href="index.php">Home</a></li>
+                      <li><a href="https://jitalumni.wordpress.com/">Blog</a></li>
+                      <li><a href="gallery.php">Glimpses</a></li>
+                      <li><a href="admission/faq.php">FAQs</a></li>
+                      <li><a href="CONTACT.php">Contact</a></li>
+                      <li><a href="sitemap.php">Website Sitemap</a></li>
+                  </ul>
+                  </div>
+                  <div class="col-sm-6">
+                      <h5>Facilities</h5>                   
+                  <ul class="list-unstyled">
+                      <li><a href="facilities/Academic.php">Academic Facilities</a></li>
+                      <li><a href="facilities/sports.php">Sports Facilities</a></li>
+                      <li><a href="facilities/health.php">Health Facilities</a></li>
+                      <li><a href="facilities/campus.php">Campus Facilities</a></li>
+                      <li><a href="facilities/innovation.php">Innovation Center</a></li>
+                      <li><a href="facilities/transport.php">Transport Facilities</a></li>
+                      
+                  </ul>
+                  </div>
+                  </div>
+              </div>
+              <div class="col-md-4">
+                  <h5>Contact Us</h5>
+                  <p>Email:  <a href="mailto:principaljitnashik@gmail.com" target="_blank">&nbsp;principaljitnashik@gmail.com</a></p>
+                  <p>Phone: <a href="tel:02532970077">&nbsp;0253-2970077</a>,<a href="tel:02532970078">&nbsp; 0253-2970078</a></p>
+                  <p>Address: <a href="https://maps.app.goo.gl/wD5wrzgvzNLYr32N6" target="_blank">&nbsp;JIT Nashik, Gangapur Road, Nashik.</a></p>
+                  <ul class="list-inline">
+                      <li class="list-inline-item"><a href="https://www.facebook.com/JITCOENSK?mibextid=ZbWKwL" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
+                      <!-- <li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li> -->
+                      <li class="list-inline-item"><a href="https://www.linkedin.com/in/jawahar-institute-technology-a9463a201/" target="_blank"><i class="fa-brands fa-linkedin"></i></a></li>
+                      <li class="list-inline-item"><a href="https://www.instagram.com/jesitmrn?igshid=NTc4MTIwNjQ2YQ%3D%3D" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
+                      <li class="list-inline-item"><a href="https://www.youtube.com/@JITNASHIK-xo7su" target="_blank"><i class="fa-brands fa-youtube"></i></a></li>
+                  </ul>
+              </div>
+          </div>
+          <div class="text-center mt-4">
+              <img src="assets/img/index/jit-white-logo.png" alt="Footer Logo" class="footer-logo">
+          </div>
+      </div>
+      <div class="text-center mt-4 footer-bottom">
+          <p>&copy; 2024 Jawahar Education Society's Institute of Technology, Management & Research, Nashik. All rights reserved.</p>
+      </div>
+  </footer>
+
+ 
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+  <script src="js/main.js"></script>
+  <script>
+  const hamBurger = document.querySelector(".toggle-btn");
+  hamBurger.addEventListener("click", function () {
+      document.querySelector("#sidebar").classList.toggle("expand");
+  });
+</script>
+
+<script>
+AOS.init({
+duration: 2000 // Duration in milliseconds (2000ms = 2 seconds)
+});
+</script>
+
+	</body>
+</html>
+
